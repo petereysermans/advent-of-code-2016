bc8a58dc45ede7620db82e7c7b84c68eb8adea49
Peter Eysermans <peter@eysermans.com>
2016-12-09 10:00:58 +0100

    Solved Part 1 of Day 1

--- AdventOfCode2016/AdventOfCode2016/Day1/BlockCalculator.cs
+++ AdventOfCode2016/AdventOfCode2016/Day1/BlockCalculator.cs
@@ -1,11 +1,98 @@
-﻿namespace AdventOfCode2016.Day1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2016.Day1
 {
     public class BlockCalculator
     {
-        public void Calculate(string moveset)
+        private Direction _direction = Direction.North;
+
+        private Dictionary<Direction, int> _numberOfBlocksPerDirection;
+
+        public BlockCalculator()
+        {
+            _numberOfBlocksPerDirection = new Dictionary<Direction, int>
+            {
+                { Direction.North, 0 },
+                { Direction.East, 0 },
+                { Direction.South, 0 },
+                { Direction.West, 0 }
+            };
+        }
+
+        private void DetermineDirection(char currentMove)
+        {
+            if (currentMove == 'R')
+            {
+                switch (_direction)
+                {
+                    case Direction.North:
+                        _direction = Direction.East;
+                        break;
+                    case Direction.East:
+                        _direction = Direction.South;
+                        break;
+                    case Direction.West:
+                        _direction = Direction.North;
+                        break;
+                    case Direction.South:
+                        _direction = Direction.West;
+                        break;
+                }
+            }
+            else
+            {
+                switch (_direction)
+                {
+                    case Direction.North:
+                        _direction = Direction.West;
+                        break;
+                    case Direction.East:
+                        _direction = Direction.North;
+                        break;
+                    case Direction.West:
+                        _direction = Direction.South;
+                        break;
+                    case Direction.South:
+                        _direction = Direction.East;
+                        break;
+                }
+            }
+        }
+
+        private void CountNumberOfBlocks(string move)
+        {
+            move = move.Trim();
+            var numberOfBlocksToMove = int.Parse(move.Substring(1));
+            DetermineDirection(move[0]);
+
+            _numberOfBlocksPerDirection[_direction] += numberOfBlocksToMove;
+        }
+
+        public int Calculate(string moveset)
         {
             var moves = moveset.Split(',');
 
+            foreach (var move in moves)
+            {
+                CountNumberOfBlocks(move);
+            }
+
+            return Math.Abs(
+                _numberOfBlocksPerDirection[Direction.North] - 
+                _numberOfBlocksPerDirection[Direction.South]) + 
+                Math.Abs(
+                _numberOfBlocksPerDirection[Direction.East] - 
+                _numberOfBlocksPerDirection[Direction.West]);
         }
+    }
+
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West
     }
 }
--- AdventOfCode2016/AdventOfCode2016/Day1/BlockCalculatorTestFixture.cs
+++ AdventOfCode2016/AdventOfCode2016/Day1/BlockCalculatorTestFixture.cs
@@ -10,7 +10,53 @@ namespace AdventOfCode2016.Day1
         public void Then_you_are_2_blocks_east()
         {
             var blockCalculator = new BlockCalculator();
-            var result = blockCalculator.Calculate();
+            var result = blockCalculator.Calculate("R2, L3");
+
+            Assert.That(result, Is.EqualTo(5));
         }
     }
+
+    [TestFixture]
+    // R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
+    public class When_the_moveset_is_r2_r2_r2
+    {
+        [Test]
+        public void Then_you_are_2_blocks_south()
+        {
+            var blockCalculator = new BlockCalculator();
+            var result = blockCalculator.Calculate("R2, R2, R2");
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+
+    [TestFixture]
+    // R5, L5, R5, R3 leaves you 12 blocks away
+    public class When_the_moveset_is_r5_l5_r5_r3
+    {
+        [Test]
+        public void Then_you_are_12_blocks_away()
+        {
+            var blockCalculator = new BlockCalculator();
+            var result = blockCalculator.Calculate("R5, L5, R5, R3");
+            Assert.That(result, Is.EqualTo(12));
+        }
+    }
+
+    [TestFixture]
+    public class Answer_the_question
+    {
+        [Test]
+        public void Test()
+        {
+            var moveset =
+                "R3, L5, R1, R2, L5, R2, R3, L2, L5, R5, L4, L3, R5, L1, R3, R4, R1, L3, R3, L2, L5, L2, R4, R5, R5, L4, L3, L3, R4, R4, R5, L5, L3, R2, R2, L3, L4, L5, R1, R3, L3, R2, L3, R5, L194, L2, L5, R2, R1, R1, L1, L5, L4, R4, R2, R2, L4, L1, R2, R53, R3, L5, R72, R2, L5, R3, L4, R187, L4, L5, L2, R1, R3, R5, L4, L4, R2, R5, L5, L4, L3, R5, L2, R1, R1, R4, L1, R2, L3, R5, L4, R2, L3, R1, L4, R4, L1, L2, R3, L1, L1, R4, R3, L4, R2, R5, L2, L3, L3, L1, R3, R5, R2, R3, R1, R2, L1, L4, L5, L2, R4, R5, L2, R4, R4, L3, R2, R1, L4, R3, L3, L4, L3, L1, R3, L2, R2, L4, L4, L5, R3, R5, R3, L2, R5, L2, L1, L5, L1, R2, R4, L5, R2, L4, L5, L4, L5, L2, L5, L4, R5, R3, R2, R2, L3, R3, L2, L5";
+
+            var blockCalculator = new BlockCalculator();
+            var result = blockCalculator.Calculate(moveset);
+
+        }
+
+    }
+
+
 }
