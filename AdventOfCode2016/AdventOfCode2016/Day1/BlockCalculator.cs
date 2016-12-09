e3e8c3395dd855d3592399dfa5af649acb24af85
Peter Eysermans <peter@eysermans.com>
2016-12-09 08:51:08 +0100

    Initial commit, started day 1

---
+++ AdventOfCode2016/AdventOfCode2016/Day1/BlockCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2016.Day1
+{
+    public class BlockCalculator
+    {
+        public void Calculate(string moveset)
+        {
+            var moves = moveset.Split(',');
+
+        }
+    }
+}
---
+++ AdventOfCode2016/AdventOfCode2016/Day1/BlockCalculatorTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2016.Day1
+{
+    [TestFixture]
+    // Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
+    public class When_the_moveset_is_r2_l3
+    {
+        [Test]
+        public void Then_you_are_2_blocks_east()
+        {
+            var blockCalculator = new BlockCalculator();
+            var result = blockCalculator.Calculate();
+        }
+    }
+}
