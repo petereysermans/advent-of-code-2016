18ded64f9f882915af40640e3295736988efc23b
Peter Eysermans <peter@eysermans.com>
2016-12-21 19:10:45 +0100

    Solved Day 5 Part 2

--- AdventOfCode2016/AdventOfCode2016/Day5/Classes/PasswordDecryptor.cs
+++ AdventOfCode2016/AdventOfCode2016/Day5/Classes/PasswordDecryptor.cs
@@ -5,6 +5,41 @@ namespace AdventOfCode2016.Day5.Classes
 {
     public class PasswordDecryptor
     {
+        public string DecryptWithPositioningFor(string doorId)
+        {
+            var index = 0;
+            var result = "";
+            var md5Calculator = new Md5HashCalculator();
+            var numberOfFoundCharacters = 0;
+            var passwordCharacters = new string[8];
+
+            while (numberOfFoundCharacters < 8)
+            {
+                var md5 = md5Calculator.Calculate(doorId + index);
+
+                if (md5.StartsWith("00000"))
+                {
+                    var position = md5.Substring(5, 1);
+                    int convertedPosition = -1;
+
+                    if (int.TryParse(position, out convertedPosition))
+                    {
+                        if (convertedPosition < 8 && 
+                            string.IsNullOrEmpty(passwordCharacters[convertedPosition]))
+                        {
+                            passwordCharacters[convertedPosition] = md5.Substring(6, 1);
+                            Debug.WriteLine(String.Join(string.Empty, passwordCharacters));
+                            numberOfFoundCharacters++;
+                        }
+                    }
+                }
+
+                index++;
+            }
+
+            return result;
+        }
+
         public string DecryptFor(string doorId)
         {
             var index = 0;
---
+++ AdventOfCode2016/AdventOfCode2016/Day5/TestFixtures/Part2TestFixture.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode2016.Day5.Classes;
+using NUnit.Framework;
+
+namespace AdventOfCode2016.Day5.TestFixtures
+{
+    [TestFixture]
+    public class When_searching_the_password_for_part_2
+    {
+        [Test]
+        public void Then_the_correct_password_is_returned_for_abc()
+        {
+            var decryptor = new PasswordDecryptor();
+            var result = decryptor.DecryptWithPositioningFor("abc");
+            Assert.That(result, Is.EqualTo("05ace8e3"));
+        }
+
+        [Test]
+        public void Then_the_password_is_returned_for_the_actual_door_id()
+        {
+            var decryptor = new PasswordDecryptor();
+            var result = decryptor.DecryptWithPositioningFor("uqwqemis");
+        }
+    }
+}
