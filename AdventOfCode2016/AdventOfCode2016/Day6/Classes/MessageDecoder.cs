db6f6462489d30c2f998a6b86e4732a03876d2c5
Peter Eysermans <peter@eysermans.com>
2016-12-22 17:44:12 +0100

    Solved Day 6 Part 2

---
+++ AdventOfCode2016/AdventOfCode2016/Day6/Classes/MessageDecoder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2016.Day6.Classes
+{
+    public class MessageDecoder
+    {
+        private string GetMostFrequentCharacter(Dictionary<string, int> countedCharacters)
+        {
+            return countedCharacters.First(x => x.Value == countedCharacters.Max(y => y.Value)).Key;
+        }
+
+        private string GetLeastFrequentCharacter(Dictionary<string, int> countedCharacters)
+        {
+            return countedCharacters.First(x => x.Value == countedCharacters.Min(y => y.Value)).Key;
+        }
+
+        private string GetCharacterForColumn(
+            string[] inputParts, 
+            int column, 
+            Func<Dictionary<string, int>, string> getCharacter )
+        {
+            var countedCharacters = new Dictionary<string, int>();
+
+            foreach(var inputPart in inputParts)
+            {
+                var character = inputPart.Trim()[column].ToString();
+
+                if (countedCharacters.ContainsKey(character))
+                {
+                    countedCharacters[character]++;
+                }
+                else
+                {
+                    countedCharacters.Add(character, 1);
+                }
+            }
+
+            return getCharacter(countedCharacters);
+        }
+
+        private string Decode(string input, Func<Dictionary<string, int>, string> getCharacter)
+        {
+            var decodedMessage = string.Empty;
+            var inputParts = input.Split('\n');
+            var numberOfCharacters = inputParts.First().Trim().Length;
+
+            for (var i = 0; i < numberOfCharacters; i++)
+            {
+                decodedMessage += GetCharacterForColumn(inputParts, i, getCharacter);
+            }
+
+            return decodedMessage;
+        }
+
+        public string DecodeUsingLeastFrequentCharacters(string input)
+        {
+            return Decode(input, GetLeastFrequentCharacter);
+        }
+
+        public string DecodeUsingMostFrequentCharacters(string input)
+        {
+            return Decode(input, GetMostFrequentCharacter);
+        }
+    }
+}
--- AdventOfCode2016/AdventOfCode2016/Day6/TestFixtures/Part2TestFixture.cs
+++ AdventOfCode2016/AdventOfCode2016/Day6/TestFixtures/Part2TestFixture.cs
@@ -1,25 +1,24 @@
 ﻿using AdventOfCode2016.Day6.Classes;
 using NUnit.Framework;
-using Input = AdventOfCode2016.Day6.Classes.Input;
 
 namespace AdventOfCode2016.Day6.TestFixtures
 {
     [TestFixture]
-    public class When_solving_part_1
+    public class When_solving_part_2
     {
         [Test]
         public void Then_the_test_case_should_pass()
         {
             var decoder = new MessageDecoder();
-            var result = decoder.DecodeUsingMostFrequentCharacters(Input.Test);
-            Assert.That(result, Is.EqualTo("easter"));
+            var result = decoder.DecodeUsingLeastFrequentCharacters(Input.Test);
+            Assert.That(result, Is.EqualTo("advent"));
         }
 
         [Test]
         public void Then_the_puzzle_can_be_solved()
         {
             var decoder = new MessageDecoder();
-            var result = decoder.DecodeUsingMostFrequentCharacters(Input.Text);
+            var result = decoder.DecodeUsingLeastFrequentCharacters(Input.Text);
         }
     }
 }
