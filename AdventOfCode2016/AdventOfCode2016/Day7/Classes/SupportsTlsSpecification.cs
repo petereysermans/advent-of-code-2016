a59d1583309bb1107dc48afe1a9c8e0ffb72415f
Peter Eysermans <peter@eysermans.com>
2016-12-23 22:25:26 +0100

    Started Day 7 Part 1

---
+++ AdventOfCode2016/AdventOfCode2016/Day7/Classes/SupportsTlsSpecification.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2016.Day7.Classes
+{
+    public class SupportsTlsSpecification
+    {
+        public bool IsSatisfied(string input)
+        {
+            var supportsTls = false;
+            var hypernetSequence = false;
+
+            for (var i = 0; i < input.Length-4; i++)
+            {
+                var char1 = input[i];
+                var char2 = input[i + 1];
+                var char3 = input[i + 2];
+                var char4 = input[i + 3];
+
+
+                if (char1 == '[')
+                {
+                    
+                }
+
+                if (char1 == char4 &&
+                    char2 == char3)
+                {
+                    supportsTls = true;
+                    break;
+                }
+            }
+
+            return supportsTls;
+        }
+    }
+}
---
+++ AdventOfCode2016/AdventOfCode2016/Day7/TestFixtures/Part1TestFixture.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode2016.Day7.Classes;
+using NUnit.Framework;
+
+namespace AdventOfCode2016.Day7.TestFixtures
+{
+    [TestFixture]
+    public class When_solving_part_1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new SupportsTlsSpecification();
+        }
+        
+        [Test]
+        public void Then_test_case_1_supports_tls()
+        {
+            _input = "abba[mnop]qrst";
+            var result = _classUnderTest.IsSatisfied(_input);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Then_test_case_2_does_not_support_tls()
+        {
+            _input = "abcd[bddb]xyyx";
+            var result = _classUnderTest.IsSatisfied(_input);
+            Assert.That(result, Is.False);
+        }
+
+        private string _input;
+        private SupportsTlsSpecification _classUnderTest;
+    }
+}
