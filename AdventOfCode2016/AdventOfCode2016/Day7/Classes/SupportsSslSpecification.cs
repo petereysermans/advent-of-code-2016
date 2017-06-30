faa24dca32f6204fc3ceb1fb9feb1fee21882f1a
Peter Eysermans <peter@eysermans.com>
2017-06-30 18:07:26 +0200

    Solved day 7 part 2

---
+++ AdventOfCode2016/AdventOfCode2016/Day7/Classes/SupportsSslSpecification.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2016.Day7.Classes
+{
+    public class SupportsSslSpecification
+    {
+        public bool IsSatisfied(string input)
+        {
+            var supportsSsl = false;
+            var hypernetSequence = false;
+
+            var foundAbaBlocks = FindAbaBlocks(input);
+
+            foreach (var abaBlock in foundAbaBlocks)
+            {
+                if (AbaBlockHasCorrespondingBabBlock(input, abaBlock))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool AbaBlockHasCorrespondingBabBlock(string input, string abaBlock)
+        {
+            var hypernetSequence = false;
+
+            for (var i = 0; i <= input.Length - 3; i++)
+            {
+                var char1 = input[i];
+                var char2 = input[i + 1];
+                var char3 = input[i + 2];
+
+                if (char1 == '[')
+                {
+                    hypernetSequence = true;
+                }
+
+                if (char1 == ']')
+                {
+                    hypernetSequence = false;
+                }
+
+                if (hypernetSequence)
+                {
+                    if (char2 == abaBlock[0] &&
+                        char1 == char3 &&
+                        char1 == abaBlock[1])
+                    {
+                        return true;
+                    }
+                }
+
+            }
+
+            return false;
+        }
+
+        private List<string> FindAbaBlocks(string input)
+        {
+            var hypernetSequence = false;
+            var foundAbaBlocks = new List<string>();
+
+            for (var i = 0; i <= input.Length-3; i++)
+            {
+                var char1 = input[i];
+                var char2 = input[i + 1];
+                var char3 = input[i + 2];
+
+                if (char1 == '[')
+                {
+                    hypernetSequence = true;
+                }
+
+                if (char1 == ']')
+                {
+                    hypernetSequence = false;
+                }
+
+                if (char1 == char3 &&
+                    char1 != char2)
+                {
+                    if (!hypernetSequence)
+                    {
+                        foundAbaBlocks.Add($"{char1}{char2}{char3}");
+                    }
+
+                }
+            }
+
+            return foundAbaBlocks;
+        }
+    }
+}
---
+++ AdventOfCode2016/AdventOfCode2016/Day7/TestFixtures/Part2TestFixture.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode2016.Day7.Classes;
+using NUnit.Framework;
+using System.Linq;
+
+namespace AdventOfCode2016.Day7.TestFixtures
+{
+    [TestFixture]
+    public class When_solving_part_2 
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new SupportsSslSpecification();
+        }
+
+        [Test]
+        public void Then_test_case_1_supports_ssl()
+        {
+            var result = _classUnderTest.IsSatisfied("aba[bab]xyz");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Then_test_case_2_does_not_support_ssl()
+        {
+            var result = _classUnderTest.IsSatisfied("xyx[xyx]xyx");
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Then_test_case_3_supports_ssl()
+        {
+            var result = _classUnderTest.IsSatisfied("aaa[kek]eke");
+            Assert.That(result, Is.True);
+
+        }
+
+        [Test]
+        public void Then_test_case_4_supports_ssl()
+        {
+            var result = _classUnderTest.IsSatisfied("zazbz[bzb]cdb");
+            Assert.That(result, Is.True);
+        }
+        
+        [Test]
+        public void Then_the_puzzle_can_be_solved()
+        {
+            var ipAddresses = Input.Text.Split('\n');
+            var numberOfValidAddresses = 
+                ipAddresses.Count(ip => _classUnderTest.IsSatisfied(ip));
+        }
+
+        private SupportsSslSpecification _classUnderTest;
+
+    }
+}
