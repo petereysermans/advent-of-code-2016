de664787c89c907cd309a49c14216c29942e2d4c
Peter Eysermans <peter@eysermans.com>
2017-10-06 21:25:42 +0200

    Start day 8

---
+++ AdventOfCode2016/AdventOfCode2016/Day8/Classes/Display.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace AdventOfCode2016.Day8.Classes
+{
+    public class Display
+    {
+        private string[,] _dots;
+
+        public Display()
+        {
+            _dots = new string[6, 50];
+
+            for (var i = 0; i < _dots.GetLength(0); i++)
+            {
+                for (var j = 0; j < _dots.GetLength(1); j++)
+                {
+                    _dots[i, j] = ".";
+                }
+            }
+        }
+
+        public void Print()
+        {
+            Console.Write(Environment.NewLine);
+
+            for (var i = 0; i < _dots.GetLength(0); i++)
+            {
+                for (var j = 0; j < _dots.GetLength(1); j++)
+                {
+                    Console.Write(_dots[i,j]);
+                }
+
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+        public void Instruct(string instruction)
+        {
+            var instructionParts = instruction.Split(' ');
+            var dimensionParts = instructionParts[1].Split('x');
+
+            if (instructionParts[0] == "rect")
+            {
+                DrawRect(instructionParts[1]);
+            }
+            else if(instructionParts[1] == "rotate")
+            {
+                
+            }
+        }
+
+        private void rotate(string parameters)
+        {
+            var parametersParts = 
+        }
+
+        private void DrawRect(string dimension)
+        {
+            var dimensionParts = dimension.Split('x');
+            var dimensionWidth = int.Parse(dimensionParts[0]);
+            var dimensionHeight = int.Parse(dimensionParts[1]);
+
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    _dots[i, j] = "#";
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode2016/AdventOfCode2016/Day8/TestFixtures/Part1TestFixture.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+using AdventOfCode2016.Day8.Classes;
+using NUnit.Framework;
+
+namespace AdventOfCode2016.Day8.TestFixtures
+{
+    [TestFixture]
+    public class When_solving_part_1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new Display();
+        }
+
+        [Test]
+        public void Then_test_case_1_should_return_the_correct_output()
+        {
+            _classUnderTest.Instruct("rect 3x2");
+            _classUnderTest.Print();
+
+             /*
+             ###...............................................
+             ###...............................................
+             ..................................................
+             ..................................................
+             ..................................................
+             ..................................................
+             */
+        }
+
+        [Test]
+        public void Then_test_case_2_should_return_the_correct_output()
+        {
+            _classUnderTest.Instruct("rect 3x2");
+            _classUnderTest.Instruct("rotate column x=1 by 1");
+            _classUnderTest.Print();
+
+             /*
+             #.#...............................................
+             ###...............................................
+             .#................................................
+             ..................................................
+             ..................................................
+             ..................................................
+             */
+        }
+
+        private Display _classUnderTest;
+    }
+}
